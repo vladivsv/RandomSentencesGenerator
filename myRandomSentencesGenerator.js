882226b7f60fc6693d6173d52a0e9e4a2fe5dbe2
vladivsv <122539600+vladivsv@users.noreply.github.com>
2023-04-19 10:32:15 +0300

    add function
    Create a Method for Getting a Random Word

--- myRandomSentencesGenerator.js
+++ myRandomSentencesGenerator.js
@@ -10,3 +10,14 @@ let nouns = ["stones", "cake", "apple", "laptop", "bikes"];
 let adverbs = ["slowly", "diligently", "warmly", "sadly", "rapidly"];
 let details = ["near the river", "at home", "in the park"];
 
+let randomNames = getRandomWord(names);
+let randomPlaces = getRandomWord(places);
+let randomVerbs = getRandomWord(verbs);
+let randomNouns = getRandomWord(nouns);
+let randomAdverbs = getRandomWord(adverbs);
+let randomDetails = getRandomWord(details);
+
+function getRandomWord(arrey) {
+    let word = arrey[Math.floor(Math.random() * arrey.length)];
+    return word;
+}
