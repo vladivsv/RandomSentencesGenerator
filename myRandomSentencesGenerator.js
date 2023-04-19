c669939d59ccddf2abeb3c50d3f78aa32ed52321
vladivsv <122539600+vladivsv@users.noreply.github.com>
2023-04-19 10:13:30 +0300

    add parametri
    words for the Sentences

---
+++ myRandomSentencesGenerator.js
@@ -0,0 +1,12 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let names = ["Peter", "Michell", "Jane", "Steve"];
+let places = ["Sofia", "Plovdiv", "Varna", "Burgas"];
+let verbs = ["eats", "holds", "sees", "plays with", "brings"];
+let nouns = ["stones", "cake", "apple", "laptop", "bikes"];
+let adverbs = ["slowly", "diligently", "warmly", "sadly", "rapidly"];
+let details = ["near the river", "at home", "in the park"];
+
